9bddaf944570c72febf2c81727dc5b5709d4bffd
Gregor <gregor.olenik@web.de>
2014-04-27 22:15:10 +0200

    FIX: fixed typo in install script

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ sudo apt-get install -y python-docopt
 grep 'snappy' ~/.bashrc > /dev/null
 if [ $? == 1 ]; then
     echo "setting path variable"
-    echo 'export PATH= $PATH:'$PWD >> ~/.bashrc
+    echo 'export PATH=$PATH:'$PWD >> ~/.bashrc
 fi
