888fd175565572e44be362d505f70492e948a7a6
Gregor <gregor.olenik@web.de>
2014-04-27 22:09:35 +0200

    add install script and README

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "installing dependencies"
+sudo apt-get install -y paraview-python
+sudo apt-get install -y python-docopt
+
+grep 'snappy' ~/.bashrc > /dev/null
+if [ $? == 1 ]; then
+    echo "setting path variable"
+    echo 'export PATH= $PATH:'$PWD >> ~/.bashrc
+fi
